1a905726ae4c81cd8fc93c7b240f28c9fcb58818
dengpei <dengpei_dp@126.com>
2020-04-03 23:42:51 +0800

    第一天学习完成，实现了如下功能20200403 1、建立数据库 2、获取数据库数据 3、增删改查

--- app01/views.py
+++ app01/views.py
@@ -1,12 +1,23 @@
+import random
+
 from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from app01.models import Student, Grade
 
 
 def hello(request):
     return render(request, 'app01/index.html')
 
 
-def world(request):
-    HttpResponse("hello,world")
+def show_grade(request):
+    student = Student.objects.get(pk=2)
+    grade = student.u_grade
+    return HttpResponse("Grade {}".format(grade.g_name))
+
+
+def show_students(request):
+    grades = Grade.objects.get(pk=2)
+    stu = grades.student_set.all()
+    return render(request, 'app01/student.html',{'stus': stu})
